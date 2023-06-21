369ea6ced5fe3b46160385682d314c94d8aa70c8
Abderaouf16 <abderaoufa88@gmail.comm>
2023-06-21 23:27:03 +0100

    finish game logic, last some styling

--- index.js
+++ index.js
@@ -1,24 +1,83 @@
 
-  function rockclick () {
-    console.log('rock')
+
+const rockButton = document.getElementById('rock')
+const scissorButton = document.getElementById('scissor')
+const paperButton =document.getElementById('paper')
+
+rockButton.addEventListener("click", function(){
+  game('rock')
+} )
+paper.addEventListener('click', function(){
+  game('paper')
+})
+scissorButton.addEventListener("click", function(){
+  game('scissor')
+})
+
+
+
+  let computerCount= 0
+  let playerCount=0 
+  let ActionText='';
+  let finalScore=''
+
+ 
+
+  function whoWins(){
+   
+    document.getElementById('finalScore').textContent = finalScore;
   }
-// get the button element
-  const rockButton = document.getElementById('rock')
-// add an event listener to it that calls handleClick when clicked
-rockButton.addEventListener("click", rockclick )
 
-function paperClick() {
-    console.log('paper')
+function updateScore(){
+   document.getElementById('playerscore').textContent = playerCount
+  document.getElementById('computerScore').textContent = computerCount
 }
-// get the paper button element
-const paper = document.getElementById('paper')
-// add an event listener to it
-paper.addEventListener('click', paperClick)
 
-function scissorClick(){
-    console.log('scissor')
+function updateText(){
+  document.getElementById('comment').textContent = ActionText;
+ 
 }
-//get the scissors button element
-const scissorButton= document.getElementById('scissor')
-//add a click handler for this one too!
-scissorButton.addEventListener("click", scissorClick)
+function game(selection){
+
+ 
+  const choices= ['rock', 'paper', 'scissor']
+  
+  const computerSelection = choices[Math.floor(Math.random() * choices.length)]  ;
+  
+
+    if (computerSelection === selection) {
+      ActionText='its a tie'
+      console.log(ActionText)
+     computerCount= computerCount
+    }else if(
+      (computerSelection =='rock' && selection=='scissor') ||
+      (computerSelection==='paper'&& selection==='rock')||
+      (computerSelection==='scissors'&& selection=== 'paper')
+    ){
+      ActionText='compter wins'
+      console.log(ActionText)
+        computerCount++
+        console.log(computerCount)
+      }else{
+        ActionText='player wins'
+        console.log(ActionText)
+       playerCount++
+      }
+
+      if(computerCount==5){
+        finalScore='computer wins the game '
+         computerCount= 0
+         playerCount=0 
+      }else if(playerCount==5){
+        finalScore='player wins the game'
+        computerCount= 0
+        playerCount=0 
+      }else{
+        finalScore=''
+      }
+    
+      updateScore()
+      updateText()
+      whoWins()
+
+}
